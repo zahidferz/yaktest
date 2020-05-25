27fef6e55a71d3ed2365f17364c05c8a745735e3
zahidferz <zahid.ferz@gmail.com>
2020-05-25 14:01:38 -0500

    CIP

--- ciserver.ps1
+++ ciserver.ps1
@@ -1,11 +1,10 @@
 $CIP=($env:CIP)
-Param(
-   [string]$collectionurl = "https://dev.azure.com/gestionix-boa", 
-   [string]$project = "gx-sandbox-pipeline",  #Variable Group which needs to be updated
-   [string]$variableName = "CIPSERVER", #The specific variable which needs to be updated
-   [string]$user = "Gestionix",
-   [string]$token = "k4wcabkpjiyrkh6ag4vyrgacm66phucsbidvxsre3nhy4de2fq2a"
-)
+$collectionurl = "https://dev.azure.com/gestionix-boa", 
+$project = "gx-sandbox-pipeline",  #Variable Group which needs to be updated
+$variableName = "CIPSERVER", #The specific variable which needs to be updated
+$user = "Gestionix",
+$token = "k4wcabkpjiyrkh6ag4vyrgacm66phucsbidvxsre3nhy4de2fq2a"
+
 #switch ($STAGE)
 #{
 #    'Dev' { $variablegroupid = '94' }
@@ -13,6 +12,7 @@ Param(
 #    'Staging' { $variablegroupid = '103' }
 #    'Production' { $variablegroupid = '104'}
 #}
+
 $variablegroupid = '102'
 # Base64-encodes the Personal Access Token (PAT) appropriately
 $base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $user,$token)))
@@ -23,10 +23,9 @@ $vgresponse.variables.$variableName.value = "$CIP"
 ####****************** update the modified object **************************
 $json = @($vgresponse) | ConvertTo-Json -Depth 99
 $updatedvg = Invoke-RestMethod -Uri $baseUri -Method Put -Body $json -ContentType "application/json" -Headers @{Authorization=("Basic {0}" -f $base64AuthInfo)}
-Cls
 write-host "==========================================================" 
 Write-host "The value of Varialbe '$variableName' is updated to" $updatedvg.variables.$variableName.value
 write-host "=========================================================="
 $STAGE=($env:STAGE)
-Write-output "Set the CIPSERVER from powershell to $CIP on the $STAGE"
+Write-output "Set the CIPSERVER is set to $CIP from the $STAGE"
 exit
