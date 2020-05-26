ba2954b6af005decc70e0ad70b94f1c94be8020d
zahidferz <zahid.ferz@gmail.com>
2020-05-25 20:25:15 -0500

    ufw allow

--- ciserver.sh
+++ ciserver.sh
@@ -1,6 +1,7 @@
 # !/bin/bash
 
 start the server and send the console and error logs on nodeserver.log
+ufw allow 8080/tcp
 npm run dev > nodeserver.log 2>&1 &
 # keep waiting until the server is started
 while ! grep "Note that the development build is not optimized." nodeserver.log
@@ -9,6 +10,6 @@ do
   sleep .1
 done
 export CIP=$(curl -s https://checkip.amazonaws.com)
-echo -e "The server has started in the $CIP:8080 \n"
+echo -e "The CI server has started in the $CIP:8080 \n"
 sleep 60
 exit 0
