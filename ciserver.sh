a9af138a987a225d6b8b12813b91b052c63ff947
zahidferz <zahid.ferz@gmail.com>
2020-05-25 20:12:10 -0500

    0.0.0.0

--- ciserver.sh
+++ ciserver.sh
@@ -3,7 +3,7 @@
 start the server and send the console and error logs on nodeserver.log
 npm run ci > nodeserver.log 2>&1 &
 # keep waiting until the server is started
-while ! grep "*App is served in production mode.*" nodeserver.log
+while ! grep "App is served in production mode." nodeserver.log
 do
   tail nodeserver.log
   sleep .1
