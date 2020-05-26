4c52dbb9cac42af57c684601919be0de75f6e6bb
zahidferz <zahid.ferz@gmail.com>
2020-05-26 08:49:54 -0500

    10.1 local

--- ciserver.sh
+++ ciserver.sh
@@ -5,8 +5,8 @@ sudo -- sh -c -e "ufw disable"
 export CIP=$(curl -s https://checkip.amazonaws.com)
 npx vue-cli-service serve --host 0.0.0.0 --port 8089 > nodeserver.log 2>&1 &
 # keep waiting until the server is started
-#while ! grep "To create a production build, run npm run build." nodeserver.log
-while [ 1 = 1 ]
+while ! grep "To create a production build, run npm run build." nodeserver.log
+#while [ 1 = 1 ]
 do
   tail nodeserver.log
   sleep .1
