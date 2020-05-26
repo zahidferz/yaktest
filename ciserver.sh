4a07099b4dd9c683e711d52e436d6808f7a27247
zahidferz <zahid.ferz@gmail.com>
2020-05-25 20:03:27 -0500

    Production

--- ciserver.sh
+++ ciserver.sh
@@ -1,14 +1,14 @@
 # !/bin/bash
 
 start the server and send the console and error logs on nodeserver.log
-npm run dev > nodeserver.log 2>&1 &
+npm run ci > nodeserver.log 2>&1 &
 # keep waiting until the server is started
-while ! grep "App running at:" nodeserver.log
+while ! grep "*App is served in production mode.*" nodeserver.log
 do
   tail nodeserver.log
   sleep .1
 done
 export CIP=$(curl -s https://checkip.amazonaws.com)
 echo -e "The server has started in the $CIP:8080 \n"
-sleep 120
+sleep 60
 exit 0
