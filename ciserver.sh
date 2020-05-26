18656375a6ab619192adcc9c6145e185d7c48eaf
zahidferz <zahid.ferz@gmail.com>
2020-05-26 08:36:24 -0500

    local jo

--- ciserver.sh
+++ ciserver.sh
@@ -3,7 +3,7 @@
 start the server and send the console and error logs on nodeserver.log
 sudo -- sh -c -e "ufw disable"
 export CIP=$(curl -s https://checkip.amazonaws.com)
-npx vue-cli-service serve --public $CIP --host 0.0.0.0 --port 8089 > nodeserver.log 2>&1 &
+npx vue-cli-service serve --host 0.0.0.0 --port 8089 > nodeserver.log 2>&1 &
 # keep waiting until the server is started
 #while ! grep "To create a production build, run npm run build." nodeserver.log
 while [ 1 = 1 ]
